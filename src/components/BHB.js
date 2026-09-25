06eb310c953f22c56443f78c963475ce6a881736
Uwe Kleine-König <uwe@kleine-koenig.org>
2026-09-25 11:21:47 +0200

    Implement fetch throttling
    The official API docs specify
    
    > It is only allowed to make a maximum of 100 requests per customer per minute to the API.
    
    So add a delay of 1000 ms between requests which is a bit more
    conservative (still more as a request is delayed 1000 ms after the *end*
    of the previous one), but we don't want to hammer the remote API and we
    don't control all requests made via other ways.

--- src/components/BHB.js
+++ src/components/BHB.js
@@ -19,11 +19,59 @@ export const authstore = reactive({
   },
 })
 
+function delay(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+function ratelimit(ms) {
+	var throttle = async function(prms, ...args) {
+		var p = new Promise(async (resolve, reject) => {
+			throttle.queue.push({ resolve: resolve, reject: reject, prms: prms, args: args });
+			await throttle.run();
+		})
+		return p;
+	};
+
+	throttle.lastexecend = Date.now();
+	throttle.queue = [];
+	throttle.current = null;
+
+	throttle.run = async function() {
+		if (throttle.current == null && throttle.queue.length > 0) {
+
+			throttle.current = delay(ms - (Date.now() - throttle.lastexecend)).then(async () => {
+				if (Date.now() - throttle.lastexecend >= ms) {
+					var o = throttle.queue.shift();
+					try {
+						var prms;
+						if (typeof(o.prms) == 'function')
+							prms = o.prms.apply(null, o.args);
+						else
+							prms = o.prms;
+
+						o.resolve(await prms);
+					} catch (error) {
+						o.reject(error);
+					}
+					throttle.lastexecend = Date.now();
+				}
+
+				throttle.current = null;
+				throttle.run();
+			});
+		}
+	}
+
+	return throttle;
+};
+
+var throttle = ratelimit(1000);
+
 export async function BHBFetch(path, params) {
 
   const body_json = { 'api_key': authstore.apikey, ...params }
 
-  const response = await fetch('/api/v1' + path, {
+  const response = await throttle(fetch, '/api/v1' + path, {
     method: 'POST',
     headers: {
       'Authorization': 'Basic ' + btoa(authstore.username + ':' + authstore.password),
